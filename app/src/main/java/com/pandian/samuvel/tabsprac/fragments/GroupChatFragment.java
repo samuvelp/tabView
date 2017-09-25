61f5dfd439417e768477a73150243943b38740bb
samuvelp <samuvelpandian@gmail.com>
2017-09-25 21:35:45 +0530

    first commit

---
+++ app/src/main/java/com/pandian/samuvel/tabsprac/fragments/GroupChatFragment.java
@@ -0,0 +1,38 @@
+package com.pandian.samuvel.tabsprac.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pandian.samuvel.tabsprac.R;
+
+public class GroupChatFragment extends Fragment {
+
+
+
+
+    public GroupChatFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_group_chat, container, false);
+    }
+
+
+}
